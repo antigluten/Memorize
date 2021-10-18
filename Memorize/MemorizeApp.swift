2c5d2492738fa1d2399068a75f1540fc1c2f3c41
Vladimir Louis <vovangus1235@gmail.com>
2021-10-18 11:18:15 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vladimir Gusev on 18.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
